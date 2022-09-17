d4f2b6bd61d4de7b50214d9b3c70f369a2b19b35
Tolga Pirim <tolgapirim25@gmail.com>
2022-09-17 14:03:53 +0300

    [GoogleAuthApp] first commit

---
+++ GoogleAuthServer/src/main/kotlin/com/prmto/Application.kt
@@ -0,0 +1,14 @@
+package com.prmto
+
+import io.ktor.server.application.*
+import com.prmto.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
