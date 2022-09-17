95e33743cefe8cd03b04fd3ebd4ccf1e04b04e6f
Tolga Pirim <tolgapirim25@gmail.com>
2022-09-17 16:02:00 +0300

    [GoogleAuthApp] setup Authentication plugin

--- GoogleAuthServer/src/main/kotlin/com/prmto/plugins/Routing.kt
+++ GoogleAuthServer/src/main/kotlin/com/prmto/plugins/Routing.kt
@@ -1,11 +1,13 @@
 package com.prmto.plugins
 
 import com.prmto.routes.rootRoute
+import com.prmto.routes.unauthorizedRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         rootRoute()
+        unauthorizedRoute()
     }
 }
