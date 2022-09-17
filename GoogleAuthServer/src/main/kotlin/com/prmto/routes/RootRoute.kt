3bc9537f9311420fbfc5019c92de61450a2d7378
Tolga Pirim <tolgapirim25@gmail.com>
2022-09-17 15:23:20 +0300

    [GoogleAuthApp] declared Endpoints

---
+++ GoogleAuthServer/src/main/kotlin/com/prmto/routes/RootRoute.kt
@@ -0,0 +1,13 @@
+package com.prmto.routes
+
+import com.prmto.domain.model.Endpoint
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Routing.rootRoute() {
+    get(Endpoint.Root.path) {
+        call.respondText("Welcome to Ktor Server!")
+    }
+
+}
